893fdb97560a15cdd106ee7166a7ac21c995acbc
zhengjb <zhengjb@shanhs.com>
2018-08-26 20:05:22 +0800

    merge

---
+++ src/components/MergeOptions.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: '哈哈哈哈哈哈飞洒'
+            }
+        },
+        created() {
+            console.log('child created')
+        },
+        mounted() {
+            console.log('child mounted')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+// import App from './App'
+import MergeOptions from './components/MergeOptions'
 
 Vue.config.productionTip = false
+
+Vue.mixin({
+  created() {
+    console.log('parent created')
+  }
+})
 
 Vue.component('app')
 /* eslint-disable no-new */
@@ -11,7 +18,5 @@ new Vue({
   el: '#app',
   // components: { App },
   // template: '<App/>'
-  render(h){
-    return h(App)
-  }
+  render: c => c(MergeOptions)
 })
