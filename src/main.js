7d45e7c52411a4f9bb0e2d616fcac6990566a5db
zhengjb <zhengjb@shanhs.com>
2018-09-19 13:22:00 +0800

    merger option

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import App from './App'
+import App from './App'
 import MergeOptions from './components/MergeOptions'
 
 Vue.config.productionTip = false
@@ -12,7 +12,7 @@ Vue.mixin({
   }
 })
 
-Vue.component('app')
+Vue.component('App', App)
 /* eslint-disable no-new */
 new Vue({
   beforeCreate() {
@@ -20,6 +20,6 @@ new Vue({
   },
   el: '#app',
   // components: { App },
-  // template: '<App/>'
-  render: c => c(MergeOptions)
+  template: '<App/>'
+  // render: c => c(MergeOptions)
 })
