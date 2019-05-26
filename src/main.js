e104577dea0ed023615494bc45b437e85e08de79
haoqi <290058005@qq.com>
2019-05-26 13:47:47 +0800

    vmodel

--- src/main.js
+++ src/main.js
@@ -15,16 +15,30 @@ Vue.config.productionTip = false
 
 // Vue.component('App', UpdateChildren)
 /* eslint-disable no-new */
+let Child = {
+  template: '<div>'
+  + '<input :value="value" @input="updateValue" placeholder="edit me">' +
+  '</div>',
+  props: ['value'],
+  methods: {
+    updateValue(e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
 
 let vm = new Vue({
   el: '#app',
-  template: '<div>'
-  + '<input v-model="message" placeholder="edit me">' +
+  template: '<div>' +
+  '<child v-model.lazy="message"></child>' +
   '<p>Message is: {{ message }}</p>' +
   '</div>',
   data() {
     return {
       message: ''
     }
+  },
+  components: {
+    Child
   }
 })
