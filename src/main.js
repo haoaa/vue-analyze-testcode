bfb318ecadc6d80cc51b7d63ac90475354b99292
haoqi <290058005@qq.com>
2019-05-26 23:27:54 +0800

    scope-slot

--- src/main.js
+++ src/main.js
@@ -11,32 +11,28 @@ import VnodeUpdate from './components/VnodeUpdate'
 import UpdateChildren from './components/UpdateChildren'
 
 Vue.config.productionTip = false
-
-let AppLayout = {
-  template: '<div class="container">' +
-  '<header><slot name="header"></slot></header>' +
-  '<main><slot>默认内容</slot></main>' +
-  '<footer><slot name="footer"></slot></footer>' +
-  '</div>'
+let Child = {
+  template: '<div class="child">' +
+  '<slot text="Hello " :msg="msg"></slot>' +
+  '</div>',
+  data() {
+    return {
+      msg: 'Vue'
+    }
+  }
 }
 
 let vm = new Vue({
   el: '#app',
   template: '<div>' +
-  '<app-layout>' +
-  '<h1 slot="header">{{title}}</h1>' +
-  '<p>{{msg}}</p>' +
-  '<p slot="footer">{{desc}}</p>' +
-  '</app-layout>' +
+  '<child>' +
+  '<template slot-scope="props">' +
+  '<p>Hello from parent</p>' +
+  '<p>{{ props.text + props.msg}}</p>' +
+  '</template>' +
+  '</child>' +
   '</div>',
-  data() {
-    return {
-      title: '我是标题',
-      msg: '我是内容',
-      desc: '其它信息'
-    }
-  },
   components: {
-    AppLayout
+    Child
   }
 })
